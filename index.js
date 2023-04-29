a5573ae0203a417ec6eb7c6fae0c7123d142a59a
조영석 <seoke_@naver.com>
2023-04-29 16:11:07 +0900

    230429 express와 mongoose 하나로 묶기

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+.then(() => {
+  console.log('Mongodb connection open!');
+}) .catch((err) => {
+  console.error(err)
+});
+
+app.set('views', path.join(__dirname), 'views');
+app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+  res.send('Woof!');
+});
+
+app.listen(3000, () => {
+  console.log('Connect!');
+});
